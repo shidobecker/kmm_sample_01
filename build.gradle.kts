87f3b306c2ac169cfc963e83d286bee165d78a78
shidobecker <juliowebdeveloper@hotmail.com>
2021-06-14 23:58:12 -0300

    Added packForXcode task

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
         classpath("com.android.tools.build:gradle:4.2.1")
     }
+
+
 }
 
 allprojects {
@@ -15,8 +17,11 @@ allprojects {
         google()
         mavenCentral()
     }
+
+
 }
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
